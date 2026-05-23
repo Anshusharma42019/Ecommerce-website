5c2d97b0d494de2b9004ab82fca19f61ae29fae2
Anshu Sharma <Anshusharma42019@gmail.com>
2026-05-23 15:27:34 +0530

    Api

--- config.js
+++ config.js
@@ -5,6 +5,11 @@ const IS_LOCAL = true;
 
 const ENV = {
   LEADS_API:    'https://backend-triven-crm.vercel.app/api/v1/leads/submit',
-  PRODUCTS_API: IS_LOCAL ? 'http://localhost:5000/api/v1' : 'https://ecommerce-backend-xi-ten.vercel.app/api/v1',
-  ORDERS_API:   IS_LOCAL ? 'http://localhost:5000/api/v1' : 'https://ecommerce-backend-xi-ten.vercel.app/api/v1',
+  PRODUCTS_API: 'https://ecommerce-backend-xi-ten.vercel.app/api/v1',
+  ORDERS_API: 'https://ecommerce-backend-xi-ten.vercel.app/api/v1',
 };
+// const ENV = {
+//   LEADS_API:    'https://backend-triven-crm.vercel.app/api/v1/leads/submit',
+//   PRODUCTS_API: IS_LOCAL ? 'http://localhost:5000/api/v1' : 'https://ecommerce-backend-xi-ten.vercel.app/api/v1',
+//   ORDERS_API:   IS_LOCAL ? 'http://localhost:5000/api/v1' : 'https://ecommerce-backend-xi-ten.vercel.app/api/v1',
+// };
